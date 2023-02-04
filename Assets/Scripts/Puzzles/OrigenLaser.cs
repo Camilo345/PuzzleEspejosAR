952417bbaf169332e1f4c59ea61326e1939bb874
Juan Camilo Bolanos <juan_cam.bolanos@uao.edu.co>
2023-02-04 13:31:44 -0500

    Line renderer

--- Assets/Scripts/Puzzles/OrigenLaser.cs
+++ Assets/Scripts/Puzzles/OrigenLaser.cs
@@ -10,45 +10,49 @@ public class OrigenLaser : MonoBehaviour
     private LineRenderer renderLaser;
     private RaycastHit hit;
     private Ray ray;
+    private bool gano=false;
     // Start is called before the first frame update
     void Start()
     {
         renderLaser = GetComponent<LineRenderer>();
         renderLaser.SetPosition(0, puntoInicio.forward);
+        lanzarRayo(transform.position, transform.right);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        lanzarRayo(transform.position, transform.right);
+       
+      
     }
 
     void lanzarRayo(Vector3 posicion, Vector3 direccion)
     {
         renderLaser.SetPosition(0, puntoInicio.position);
-        for(int i = 0; i < rebotes; i++)
+        for(int i = 1; i < rebotes; i++)
         {
             ray = new Ray(posicion, direccion);
-            if(Physics.Raycast(ray,out hit, 100))
+            Debug.DrawRay(posicion, direccion * hit.distance, Color.green);
+            if (Physics.Raycast(ray,out hit, 100))
             {
-                Debug.DrawRay(posicion, direccion*hit.distance, Color.green);
+              
                 posicion = hit.point;
                 direccion = Vector3.Reflect(ray.direction, hit.normal);
+                renderLaser.SetPosition(i, posicion);
                 Debug.DrawRay(posicion, direccion * hit.distance, Color.green);
-                 renderLaser.SetPosition(i + 1, hit.point);
-                /* if (!hit.transform.CompareTag("Espejo"))
-                 {
-                     for (int j = (i + 1); j <= rebotes; j++)
-                     {
-                         renderLaser.SetPosition(j, hit.point);
-                     }
-                     break;
-                 }*/
                 if (hit.collider.gameObject.CompareTag("Objetivo"))
                 {
-                    Debug.Log("CheckMate");
+                    Debug.Log("checkmate");
+                   
+                    break;
                 }
+                if (hit.transform.tag!="Espejo")
+                 {
+                    Debug.Log("Pared");
+                    renderLaser.positionCount--;
+                    break;
+                 }
+               
             }
         }
     }
